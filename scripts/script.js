e78c981bcd6c2fdee361546af2029f72a54a39b1
Jan Smutný <u18.426@pslib.cloud>
2020-10-20 23:48:36 +0200

    idk

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+let ham = false; //false == zatáhnuto
+let harburgr;
+let list;
+let hamburger;
+let aList;
+doTheHamburger = () => {
+    hamburger = document.getElementById("hamburger");
+    hamburger.classList.toggle("is-active"); /* prohození stavu*/
+    if(ham) { //pokud je roztáhnuto
+        ham = false;
+        list.style.display = "none";
+    }
+    else { //pokud je zatáhnuto
+        ham = true;
+        list.style.display = "block";
+    }
+}
+doAutoHamburger = () => {
+    hamburger = document.getElementById("hamburger");
+    if(window.innerWidth > 560) {
+        ham = false;
+        list.style.display = "none";
+        hamburger.classList.remove("is-active");
+    }
+}
+specialABehav = (e) => {
+    e.preventDefault();
+    let href = e.target.getAttribute("href");
+    const element = document.querySelector(href)
+    const topPos = element.getBoundingClientRect().top + window.pageYOffset
+    window.scrollTo(0, topPos -90);
+}
+init = () => {
+    window.scrollTo(0, 0-1);
+    harburgr = document.getElementById("hamburger");
+    list = document.getElementById("normal-list");
+    aList = document.getElementsByClassName("anchor");
+    harburgr.addEventListener("click", doTheHamburger);
+    window.addEventListener("resize", doAutoHamburger);
+    for(let i = 0; i < aList.length; i++) {
+        aList[i].addEventListener("click", specialABehav);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", init);
