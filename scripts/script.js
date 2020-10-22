fbf367bf086826791851d536bb79ef7577f3f7a4
Jan Smutný <u18.426@pslib.cloud>
2020-10-22 16:39:25 +0200

    sipka

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ let harburgr;
 let list;
 let hamburger;
 let aList;
+let clickable;
 doTheHamburger = () => {
     hamburger = document.getElementById("hamburger");
     hamburger.classList.toggle("is-active"); /* prohození stavu*/
@@ -26,19 +27,44 @@ doAutoHamburger = () => {
 specialABehav = (e) => {
     e.preventDefault();
     let href = e.target.getAttribute("href");
-    const element = document.querySelector(href)
+    const element = document.querySelector(href);
     const topPos = element.getBoundingClientRect().top + window.pageYOffset
-    window.scrollTo(0, topPos -90);
+    if(window.innerWidth <= 560) {
+        window.scrollTo(0, topPos -250);
+    }
+    else {
+        window.scrollTo(0, topPos -90);
+    }
+} 
+secretText = (e) => {
+    let id = e.target.getAttribute("class").split(" ")[0];
+    let secret = document.getElementById(id);  
+    if(window.getComputedStyle(secret, null).display == "block") {
+        secret.style.display = "none"
+        e.target.style.transform = "rotate(0deg)"
+    }
+    else {
+        secret.style.display = "block";
+        e.target.style.transform = "rotate(180deg)"
+    }
+    console.log(secret);  
+    console.log(window.getComputedStyle(secret, null).display);
 }
+
 init = () => {
     window.scrollTo(0, 0-1);
     harburgr = document.getElementById("hamburger");
     list = document.getElementById("normal-list");
     aList = document.getElementsByClassName("anchor");
+    showMore = document.querySelectorAll(".showMore");
+
     harburgr.addEventListener("click", doTheHamburger);
     window.addEventListener("resize", doAutoHamburger);
     for(let i = 0; i < aList.length; i++) {
         aList[i].addEventListener("click", specialABehav);
+    }
+    for(let i = 0; i < showMore.length; i++) {
+        showMore[i].addEventListener("click", secretText);
     }
 }
 
